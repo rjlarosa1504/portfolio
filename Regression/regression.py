5cd41fc025e9865f95a65454ece817efb9a71c5c
Renzo La Rosa <renzo.j.larosa@gmail.com>
2022-05-24 19:59:41 -0400

    Add test data results

--- Regression/regression.py
+++ Regression/regression.py
@@ -27,13 +27,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_
 # Simple Linear Regression
 reg = LinearRegression()
 reg.fit(X_train, y_train)
-linearScore = reg.score(X_train, y_train)
+linearScoreTrain = reg.score(X_train, y_train)
 
 # plt.plot(y_train, reg.predict(X_train), 'o', color = 'black')
 # plt.xlabel("Actual Charges ($)")
 # plt.ylabel("Predicted Charges ($)")
 # plt.title("Simple Linear Regression")
 # plt.show()
+from sklearn.metrics import r2_score
+
+linearScoreTest = r2_score(reg.predict(X_test), y_test)
 
 # KNN Regression
 from sklearn.neighbors import KNeighborsRegressor
@@ -52,10 +55,11 @@ for i in range(2, 21, 2):
 
 knn = KNeighborsRegressor(n_neighbors=5)
 knn.fit(X_train, y_train)
-knnScore = knn.score(X_train, y_train)
+knnScoreTrain = knn.score(X_train, y_train)
 
 # Optimal n_neighbors results in overfitting
 # KNN not optimal since high results result from very low n_neighbors
+knnScoreTest = r2_score(knn.predict(X_test), y_test)
 
 # Random Forest Regression
 from sklearn.ensemble import RandomForestRegressor
@@ -75,7 +79,9 @@ for i in range(2, 21, 2):
 # High R-squared value even with moderate number of leaves
 randomForest = RandomForestRegressor(n_estimators =  30, min_samples_leaf = 5, random_state = 42)
 randomForest.fit(X_train, y_train)
-rfScore = randomForest.score(X_train, y_train)
+rfScoreTrain = randomForest.score(X_train, y_train)
+
+rfScoreTest = r2_score(randomForest.predict(X_test), y_test)
 
 # Using Boosting (AdaBoost)
 
@@ -83,13 +89,22 @@ from sklearn.ensemble import AdaBoostRegressor
 
 boost = AdaBoostRegressor(random_state=42, n_estimators=30)
 boost.fit(X_train, y_train)
-boostScore = boost.score(X_train, y_train)
+boostScoreTrain = boost.score(X_train, y_train)
 
-print("Top regression results")
-print("Linear Regression:\t", str(linearScore))
-print("KNN\t", str(knnScore))
-print("Random Forest\t", str(rfScore))
-print("AdaBoost\t", str(boostScore))
+boostScoreTest = r2_score(boost.predict(X_test), y_test)
+
+print("Training Data: Top regression results")
+print("Linear Regression:\t", str(linearScoreTrain))
+print("KNN\t", str(knnScoreTrain))
+print("Random Forest\t", str(rfScoreTrain))
+print("AdaBoost\t", str(boostScoreTrain))
+
+print("Testing Data: Top regression results")
+print("Linear Regression:\t", str(linearScoreTest))
+print("KNN\t", str(knnScoreTest))
+print("Random Forest\t", str(rfScoreTest))
+print("AdaBoost\t", str(boostScoreTest))
+
 
 """
 Random Forest provides the best regression predictions with a score of {rfScore} and leaf size of 5.
